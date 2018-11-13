1ed9fbd069923b1c92a3e12e23f5cc5fa169e58f
Giuseppe Nebbione <gnebbia@users.noreply.github.com>
2018-11-13 20:17:52 +0100

    Added performance measure of algorithms

--- classification.R
+++ classification.R
@@ -1,6 +1,10 @@
+# Example of Classification, using the logistic regression model and
+# predicting Default as a function of all the other variables
+
 # Import necessary libraries
 library(dplyr)
 library(readr)
+library(caret)
 
 
 ds = read_csv("companies_data.csv")
@@ -11,14 +15,39 @@ ds$Default    = as.factor(ds$Default)
 ds$nace       = as.factor(ds$nace)
 ds$nace_group = as.factor(ds$nace_group)
 
-classification_model = glm(ROI ~ ., ds, )
+# Remove the nace column, it is a categorical variable too big, and should need
+# a specific particular treatment
+ds = ds %>% select(-c('nace'))
+
+# Split dataset into training and testing
+## In this case we are performing a 70/30 split, so we will
+## use the 70% of the dataset to build the model (training phase)
+## and the remaining 30% of the dataset to assess the performance of the builded
+## model (testing phase)
+train.index <- createDataPartition(ds$Default, p = .7, list = FALSE)
+
+ds.train = ds[train.index, ]
+ds.test  = ds[-train.index, ]
+
+
+# We build a classification model which can be used to predict Default, so ROI
+# will be our dependent variable, change ROI to whatever other real number variable
+# to create a model which is predicting something else
+classification_model = glm(Default ~ ., family=binomial(), data=ds.train)
 
 # Visualize all the characteristics of the regression model
-summary(regression_model)
+summary(classification_model)
 
 
 # Perform prediction on the test set
+# remember to set the type='response' in order to obrain probabilities,
+# since by default it will predict log odds of probability
+predicted_Y_p = predict(classification_model, ds.test, type="response")
+
+# We convert probability prediction to a categorical variable, (0 or 1),
+# in order to compare it to the real response data contained in ds.test$Default
+predicted_Y   = factor(ifelse(predicted_Y_p > 0.5, 1, 0), levels=c(0,1))
 
 # Measure performance of the model in terms of F1-Score 
+result <- confusionMatrix(predicted_Y, ds.test$Default, mode="prec_recall")
 
-# Measure performance of the model in terms of precision recall
--- regression.R
+++ regression.R
@@ -1,6 +1,11 @@
+# Example of Regression, using the classical linear regression model and
+# predicting ROI as a function of all the other variables
+
 # Import necessary libraries
 library(dplyr)
 library(readr)
+library(caret)
+library(modelr)
 
 
 ds = read_csv("companies_data.csv")
@@ -11,14 +16,36 @@ ds$Default    = as.factor(ds$Default)
 ds$nace       = as.factor(ds$nace)
 ds$nace_group = as.factor(ds$nace_group)
 
-regression_model = lm(ROI ~ ., ds)
+# Remove the nace column, it is a categorical variable too big, and should need
+# a specific particular treatment
+ds = ds %>% select(-c('nace'))
+
+# Split dataset into training and testing
+## In this case we are performing a 70/30 split, so we will
+## use the 70% of the dataset to build the model (training phase)
+## and the remaining 30% of the dataset to assess the performance of the builded
+## model (testing phase)
+train.index <- createDataPartition(ds$ROI, p = .7, list = FALSE)
+
+ds.train = ds[train.index, ]
+ds.test  = ds[-train.index, ]
+
+
+# We build a regression model which can be used to predict ROI, so ROI
+# will be our dependent variable, change ROI to whatever other real number variable
+# to create a model which is predicting something else
+regression_model = lm(ROI ~ ., ds.train)
 
 # Visualize all the characteristics of the regression model
 summary(regression_model)
 
 
 # Perform prediction on the test set
+predicted_Y <- predict(regression_model, ds.test) 
 
-# Measure performance of the model in terms of F1-Score 
+# Measure performance of the model
+rmse(regression_model, ds.test)
+rsquare(regression_model, ds.test)
 
-# Measure performance of the model in terms of precision recall
+
+
