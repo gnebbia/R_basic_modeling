fcb45229de8052c04f3120a6df3435bcfa433991
Giuseppe Nebbione <gnebbia@users.noreply.github.com>
2018-12-12 16:43:58 +0100

    Updated Classification Neural

--- advanced_modeling_neural_regression.R
+++ advanced_modeling_neural_regression.R
@@ -7,24 +7,14 @@ library(caret)
 library(neuralnet)
 
 
-ds = read_csv("companies_data.csv")
-
-# Preprocess Dataset
-## Converting appropriately categories into categorical variables (factors)
-ds$Default    = as.factor(ds$Default)
-ds$nace       = as.factor(ds$nace)
-ds$nace_group = as.factor(ds$nace_group)
-
-# Remove the nace column, it is a categorical variable too big, and should need
-# a specific particular treatment
-ds = ds %>% select(-c('nace'))
+ds = read_csv("~/Desktop/exchanges.csv")
 
 # Split dataset into training and testing
 ## In this case we are performing a 70/30 split, so we will
 ## use the 70% of the dataset to build the model (training phase)
 ## and the remaining 30% of the dataset to assess the performance of the builded
 ## model (testing phase)
-train.index <- createDataPartition(ds$Default, p = .8, list = FALSE)
+train.index <- createDataPartition(ds$Date, p = .8, list = FALSE)
 
 ds.train = ds[train.index, ]
 ds.test  = ds[-train.index, ]
@@ -37,7 +27,7 @@ ds.test  = ds[-train.index, ]
 
 # Here we set the parameters of the neural network, so it should have 5 hidden
 # units and a learning rate of 0.2
-num_hidden_layers = 5
+num_hidden_layers
 t.grid = expand.grid(size=num_hidden_layers, decay=0.2)
 
 # Now we build the model and apply the following preprocessing steps:
@@ -45,6 +35,16 @@ t.grid = expand.grid(size=num_hidden_layers, decay=0.2)
 # These preprocessing steps on data improve the overall neural network
 # classifier
 model <- train(Default ~ ., data = ds.train, preProcess = c("pca","scale","center"), method = "nnet", linout = 0, tuneGrid = t.grid)
+
+# Plot the neural network model
+n = names(ds.train)
+formula <- as.formula(paste("Default ~", paste(n[!n %in% "Default"], collapse = " + ")))
+
+
+
+# Plot the neural network hidden = 5, this is just the graph of the network used
+# for the classification problem
+plot(model)
 
 
 # Visualize all the characteristics of the regression model
@@ -58,8 +58,8 @@ predicted_Y = predict(model, ds.test)
 
 
 # Measure performance of the model in terms of F1-Score 
-result0 <- confusionMatrix(predicted_Y, ds.test$Default, mode="prec_recall", positive="0")
-result1 <- confusionMatrix(predicted_Y, ds.test$Default, mode="prec_recall", positive="1")
+result <- confusionMatrix(predicted_Y, ds.test$Default, mode="prec_recall", positive="0")
+result <- confusionMatrix(predicted_Y, ds.test$Default, mode="prec_recall", positive="1")
 
 
 
